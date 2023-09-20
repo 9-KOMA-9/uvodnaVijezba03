7243e02ee2c12f8509d13d762cd5451879ba02d5
9-KOMA-9 <145533890+9-KOMA-9@users.noreply.github.com>
2023-09-20 09:27:55 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uvodnaViježba03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y, z;
+            Console.Write(" unesite prvi broj: ");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" unesite drugi broj: ");
+            y = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write(" unesite treci broj: ");
+            z = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(" prvi upisani broj je: " + x);
+            Console.WriteLine(" drugi upisani broj je: " + y);
+            Console.WriteLine(" treći upisani broj je: " + z);
+
+            if (x > y && x > z)
+            {
+                if ((y * y) + (z * z) == (x * x))
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut je pravokutan ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut nije pravokutan ");
+                }
+
+            }
+
+            if (y > x  && y > z)
+            {
+                if ((x * x) + (z * z) == (y * y))
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut je pravokutan ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut nije pravokutan ");
+                }
+
+            }
+
+            if (z > y && z > x)
+            {
+                if ((y * y) + (x * x) == (z * z))
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut je pravokutan ");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.Write(" trokut nije pravokutan ");
+                }
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
